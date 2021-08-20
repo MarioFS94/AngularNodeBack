37dfeb318e706092f4a654b4be08c3bc10d9a8be
MarioFS <mario.fernandezsuarez@plexus.es>
2021-08-20 13:19:08 +0200

    add cors

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
 //creamos el servidor
 const app = express();
 
 //conectamos a la BD
 conectarDB();
-
+//lo instalamos y es por que utilizamos diferente puerto en back y front
+app.use(cors());
 //habilitamos para que nos puedan enviar json
 app.use(express.json());
 
-app.use('/api/productos', require('./routes/productos'));
-
 //definimos ruta principal
+app.use('/api/productos', require('./routes/productos'));
 /*app.get('/', (request, response) => {
     response.send('Hola mundo');
 });*/
